adc2d223f1e6d6620b9fe27029dec90f783a0708
Ubuntu <ubuntu@ip-172-31-81-244.ec2.internal>
2022-02-09 01:47:43 +0000

    add serilization when forwarding function

--- client/pheromone/test_serialization.py
+++ client/pheromone/test_serialization.py
@@ -6,14 +6,14 @@ import time
 import os
 
 # client = PheromoneClient('a1f7912a3bf404bbcba15e1367345190-1317238364.us-east-1.elb.amazonaws.com', 'ad0dc9175284b4cdf80bf7c8ed5ffa8c-352615241.us-east-1.elb.amazonaws.com', '18.212.247.15', thread_id=0)
-client = PheromoneClient('127.0.0.1', '127.0.0.1', '127.0.0.1', thread_id=0)
+client = PheromoneClient('a46163c73e16b416396314ca485f15d5-1622429709.us-east-1.elb.amazonaws.com', 'a8940347b05664c6ea9294fcf41d6e21-1576752125.us-east-1.elb.amazonaws.com', '54.88.41.113', thread_id=0)
 
-app_name = 'func-chain'
+app_name = 'func-chain-new5'
 
-dependency = (['inc'], ['inc'], DIRECT)
-client.register_app(app_name, ['inc'], [dependency])
+dependency = (['first'], ['second'], DIRECT)
+client.register_app(app_name, ['first', 'second'], [dependency])
 
-res = client.call_app(app_name, [('inc', [[1,2,3,4,5]])], synchronous=True)
+res = client.call_app(app_name, [('first', [[1,2,3,4,5]])], synchronous=True)
 
 print(res)
 
