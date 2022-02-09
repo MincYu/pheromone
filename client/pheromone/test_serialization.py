73cc937141f25742db4caaa3c6560d856ba893d5
Ubuntu <ubuntu@ip-172-31-81-244.ec2.internal>
2022-02-09 02:10:02 +0000

    test update

--- client/pheromone/test_serialization.py
+++ client/pheromone/test_serialization.py
@@ -8,12 +8,14 @@ import os
 # client = PheromoneClient('a1f7912a3bf404bbcba15e1367345190-1317238364.us-east-1.elb.amazonaws.com', 'ad0dc9175284b4cdf80bf7c8ed5ffa8c-352615241.us-east-1.elb.amazonaws.com', '18.212.247.15', thread_id=0)
 client = PheromoneClient('a46163c73e16b416396314ca485f15d5-1622429709.us-east-1.elb.amazonaws.com', 'a8940347b05664c6ea9294fcf41d6e21-1576752125.us-east-1.elb.amazonaws.com', '54.88.41.113', thread_id=0)
 
-app_name = 'func-chain-new5'
+app_name = 'func-chain-new6'
 
 dependency = (['first'], ['second'], DIRECT)
 client.register_app(app_name, ['first', 'second'], [dependency])
-
-res = client.call_app(app_name, [('first', [[1,2,3,4,5]])], synchronous=True)
+param = []
+for i in range(100000):
+    param.append(i)
+res = client.call_app(app_name, [('first', [param])], synchronous=True)
 
 print(res)
 
--- examples/cpp/function_chain/upload_chain.sh
+++ examples/cpp/function_chain/upload_chain.sh
@@ -6,3 +6,12 @@ for pod in ${func_nodes[@]}; do
 done
 
 wait
+
+set -x
+
+func_nodes=$(kubectl get pod | grep func | cut -d " " -f 1 | tr -d " ")
+for pod in ${func_nodes[@]}; do
+    kubectl cp second.so $pod:/dev/shm -c local-sched &
+done
+
+wait
